005b93f626830ed85afac0b4018932a1d212ff98
Camillashodi <camillashodi@gmail.com>
2022-06-06 15:24:56 +0300

    all without hard-slider

--- source/js/main.js
+++ source/js/main.js
@@ -8,6 +8,66 @@ window.addEventListener('DOMContentLoaded', () => {
   // Utils
   // ---------------------------------
 
+  let settingsList = document.querySelectorAll('[data-time-settings]');
+  let contentList = document.querySelectorAll('[data-list]');
+
+  function removeClass(array) {
+    array.forEach((element) => {
+      element.classList.remove('is-active');
+    });
+  }
+
+  settingsList.forEach((setting) => {
+    setting.addEventListener('click', function (e) {
+      e.preventDefault();
+      if (!setting.classList.contains('is-active')) {
+        removeClass(settingsList);
+        removeClass(contentList);
+        let chooseSetting = e.target;
+        let chooseSettingValue = chooseSetting.dataset.timeSettings;
+        let choosenContent = document.querySelector('[data-list="' + chooseSettingValue + '"]');
+        choosenContent.classList.add('is-active');
+        chooseSetting.classList.add('is-active');
+      }
+    });
+  });
+
+  // -----------------slider---------------
+
+  let btnRight = document.querySelector('.slider__button--next');
+  let btnLeft = document.querySelector('.slider__button--previous');
+  let slides = document.querySelectorAll('.slider__item');
+  let index = 0;
+
+  btnRight.addEventListener('click', function () {
+    ++index;
+    if (index >= slides.length) {
+      slides[index - 1].classList.remove('is-active');
+      index = 0;
+      slides[index].classList.add('is-active');
+    } else {
+      slides[index - 1].classList.remove('is-active');
+      slides[index].classList.add('is-active');
+    }
+  });
+
+  btnLeft.addEventListener('click', function () {
+    --index;
+    if (index < 0) {
+      index = 0;
+      slides[index].classList.remove('is-active');
+      index = slides.length - 1;
+      slides[index].classList.add('is-active');
+    } else {
+      slides[index + 1].classList.remove('is-active');
+      slides[index].classList.add('is-active');
+    }
+  });
+
+  // ---------------carousel---------------
+
+
+
   iosVhFix();
 
   // Modules
